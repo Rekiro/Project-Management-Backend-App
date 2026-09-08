f3b63adc5ce88fa756b5835edae54c7e293d8b05
Rekiro <81427486+Rekiro@users.noreply.github.com>
2026-09-09 04:03:12 +0530

    Added all other DB models

---
+++ src/models/projectMember.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+import { AvailableUserRole, UserRolesEnum } from "../utils/constants";
+
+const projectMemberSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        project: {
+            type: Schema.Types.ObjectId,
+            ref: "Project",
+            required: true,
+        },
+        role: {
+            type: String,
+            enum: AvailableUserRole,
+            default: UserRolesEnum.MEMBER,
+        },
+    },
+    { timestamps: true },
+);
+
+export const ProjectMemeber = mongoose.model(
+    "ProjectMember",
+    projectMemberSchema,
+);
